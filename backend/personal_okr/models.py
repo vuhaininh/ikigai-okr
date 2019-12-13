6c1c7694576863ce0d7aba25e3007d53600407ef
Vu Hai Ninh <haininh.vu89@gmail.com>
2019-12-13 15:48:08 +0700

    init

---
+++ backend/personal_okr/models.py
@@ -0,0 +1,39 @@
+from django.db import models
+from django.conf import settings
+
+
+class Tag(models.Model):
+    """Tag to be used for a objective"""
+    name = models.CharField(max_length=255)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Objective(models.Model):
+    """Objective of an OKR"""
+    description = models.CharField(max_length=500)
+    finished_date = models.DateField()
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+    tags = models.ManyToManyField(Tag)
+
+    def __str__(self):
+        return self.description
+
+
+class KeyResult(models.Model):
+    """Key Result of an Objective"""
+    objective = models.ForeignKey(
+        Objective, related_name='key_results', on_delete=models.CASCADE)
+    description = models.CharField(max_length=500)
+    finished_date = models.DateField()
+
+    def __str__(self):
+        return self.description
