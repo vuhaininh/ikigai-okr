fb97556106f1fd46c533c5be7d50f432adcb2276
Vu Hai Ninh <haininh.vu89@gmail.com>
2019-12-15 20:24:00 +0700

    Add Relay

--- backend/personal_okr/schema.py
+++ backend/personal_okr/schema.py
@@ -1,77 +1,39 @@
+from graphene import relay, ObjectType
+from graphene_django import DjangoObjectType
+from graphene_django.filter import DjangoFilterConnectionField
 import graphene
-from graphene_django.types import DjangoObjectType
-from .models import Tag, KeyResult, Objective
+from personal_okr.models import Tag, Objective, KeyResult
 
 
-class TagType(DjangoObjectType):
+class TagNode(DjangoObjectType):
     class Meta:
         model = Tag
+        filter_fields = ['name']
+        interfaces = (relay.Node,)
 
 
-class ObjectiveType(DjangoObjectType):
+class ObjectiveNode(DjangoObjectType):
     class Meta:
         model = Objective
+        filter_fields = {
+            'description': ['exact', 'icontains', 'istartswith'], }
+        interfaces = (relay.Node,)
 
 
-class KeyResultType(DjangoObjectType):
+class KeyResultNode(DjangoObjectType):
     class Meta:
         model = KeyResult
+        filter_fields = {
+            'description': ['exact', 'icontains', 'istartswith'], }
+        interfaces = (relay.Node,)
 
 
-class Query(object):
-    tag = graphene.Field(TagType,
-                         id=graphene.Int(),
-                         name=graphene.String())
-    all_tags = graphene.List(TagType)
-    objective = graphene.Field(ObjectiveType,
-                               id=graphene.Int(),
-                               description=graphene.String(),
-                               finished_date=graphene.Date())
-    all_objectives = graphene.List(ObjectiveType)
-    key_result = graphene.Field(KeyResultType,
-                                id=graphene.Int(),
-                                description=graphene.Int(),
-                                finished_date=graphene.Date())
-    all_key_results = graphene.List(KeyResultType)
+class Query(graphene.ObjectType):
+    tag = relay.Node.Field(TagNode)
+    all_tags = DjangoFilterConnectionField(TagNode)
 
-    def resolve_all_tags(self, info, **kwargs):
-        return Tag.objects.all()
+    objective = relay.Node.Field(ObjectiveNode)
+    all_objectives = DjangoFilterConnectionField(ObjectiveNode)
 
-    def resolve_tag(self, info, **kwargs):
-        id = kwargs.get('id')
-        name = kwargs.get('name')
-        if id is not None:
-            return Tag.objects.get(pk=id)
-        if name is not None:
-            return Tag.objects.get(name=name)
-        return None
-
-    def resolve_all_objectives(self, info, **kwargs):
-        return Objective.objects.all()
-
-    def resolve_objective(self, info, **kwargs):
-        id = kwargs.get('id')
-        description = kwargs.get('description')
-        finished_date = kwargs.get('finished_date')
-        if id is not None:
-            return Objective.objects.get(pk=id)
-        if description is not None:
-            return Objective.objects.get(description=description)
-        if finished_date is not None:
-            return Objective.objects.get(finished_date=finished_date)
-        return None
-
-    def resolve_all_key_results(self, info, **kwargs):
-        return KeyResult.objects.select_related('objective').all()
-
-    def resolve_key_result(self, info, **kwargs):
-        id = kwargs.get('id')
-        description = kwargs.get('description')
-        finished_date = kwargs.get('finished_date')
-        if id is not None:
-            return KeyResult.objects.get(pk=id)
-        if description is not None:
-            return KeyResult.objects.get(description=description)
-        if finished_date is not None:
-            return KeyResult.objects.get(finished_date=finished_date)
-        return None
+    key_result = relay.Node.Field(KeyResultNode)
+    all_key_results = DjangoFilterConnectionField(KeyResultNode)
--- backend/users/schema.py
+++ backend/users/schema.py
@@ -1,36 +1,18 @@
+from graphene import relay, ObjectType
+from graphene_django import DjangoObjectType
+from graphene_django.filter import DjangoFilterConnectionField
 import graphene
-from graphene_django.types import DjangoObjectType
-from .models import User
+
+from users.models import User
 
 
-class UserType(DjangoObjectType):
+class UserNode(DjangoObjectType):
     class Meta:
         model = User
+        filter_fields = ['first_name', 'last_name', 'email']
+        interfaces = (relay.Node,)
 
 
-class Query(object):
-    all_users = graphene.List(UserType)
-    user = graphene.Field(UserType,
-                          id=graphene.Int(),
-                          first_name=graphene.String(),
-                          last_name=graphene.String(),
-                          email=graphene.String(),
-                          )
-
-    def resolve_all_users(self, info, **kwargs):
-        return User.objects.all()
-
-    def resolve_user(self, info, **kwargs):
-        id = kwargs.get('id')
-        first_name = kwargs.get('first_name')
-        last_name = kwargs.get('last_name')
-        email = kwargs.get('email')
-        if id is not None:
-            return User.objects.get(pk=id)
-        if first_name is not None:
-            return User.objects.get(first_name=first_name)
-        if last_name is not None:
-            return User.objects.get(last_name=last_name)
-        if email is not None:
-            return User.objects.get(email=email)
-        return None
+class Query(graphene.ObjectType):
+    user = relay.Node.Field(UserNode)
+    all_users = DjangoFilterConnectionField(UserNode)
